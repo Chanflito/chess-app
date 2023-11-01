65d9044e153d5860b8eb319b871feca70b091292
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2023-11-01 16:37:51 -0300

    refactor: change confused method name.

--- app/src/main/java/validator/UnidirectionalMovementValidator.java
+++ app/src/main/java/validator/UnidirectionalMovementValidator.java
@@ -19,16 +19,16 @@ public class UnidirectionalMovementValidator implements MovementValidator {
     @Override
     public boolean isValid(Movement movement, Board board) {
         Position position= new Position(movement.getFrom().x()+ rows,movement.getFrom().y()+ columns);
-        if (checkIfIsInsideBoard(position, board)) return false;
+        if (checkIfNotInsideOfBoard(position, board)) return false;
         while (!position.equals(movement.getTo())){
             position= new Position(position.x()+ rows,position.y()+ columns);
-            if (checkIfIsInsideBoard(position, board)) return false;
+            if (checkIfNotInsideOfBoard(position, board)) return false;
         }
         return true;
     }
 
 
-    private boolean checkIfIsInsideBoard(Position position,Board board){
+    private boolean checkIfNotInsideOfBoard(Position position, Board board){
         return position.y() < 0 || position.y() >= board.getSizeOfColumns()
                 || position.x() < 0 || position.x() >= board.getSizeOfRows();
     }
